fadc7781b680d1327e66a0093e3a770446436500
chungkwonryu <chungkwonryu.dev@gmail.com>
2022-12-11 00:39:06 +0900

    blahsome icon, favicon 추가

--- src/components/Footer/Copyright.jsx
+++ src/components/Footer/Copyright.jsx
@@ -4,6 +4,12 @@ const Copyright = ({ copyright }) => {
   return (
     <div className="m-2 text-stone-400">
       <small>{copyright}</small>
+      <br />
+      <a
+        href="https://www.flaticon.com/free-icons/flower"
+        title="flower icons">
+        <small>Flower icons created by Freepik - Flaticon</small>
+      </a>
     </div>
   );
 };
--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -12,7 +12,24 @@ const MainLayout = ({ children, hasFooter = true }) => (
     <Helmet htmlAttributes={{ lang: siteConfig.siteLang }}>
       <meta name="description" content={siteConfig.siteDescription} />
       <meta name="google-site-verification" content={siteConfig.googleSearchConsoleContent} />
-      <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
+      <link rel="shortcut icon" type="image/x-icon" href="/icons/favicon.ico" />
+      <link rel="apple-touch-icon" sizes="57x57" href="/icons/apple-icon-57x57.png" />
+      <link rel="apple-touch-icon" sizes="60x60" href="/icons/apple-icon-60x60.png" />
+      <link rel="apple-touch-icon" sizes="72x72" href="/icons/apple-icon-72x72.png" />
+      <link rel="apple-touch-icon" sizes="76x76" href="/icons/apple-icon-76x76.png" />
+      <link rel="apple-touch-icon" sizes="114x114" href="/icons/apple-icon-114x114.png" />
+      <link rel="apple-touch-icon" sizes="120x120" href="/icons/apple-icon-120x120.png" />
+      <link rel="apple-touch-icon" sizes="144x144" href="/icons/apple-icon-144x144.png" />
+      <link rel="apple-touch-icon" sizes="152x152" href="/icons/apple-icon-152x152.png" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-icon-180x180.png" />
+      <link rel="icon" type="image/png" sizes="192x192"  href="/icons/android-icon-192x192.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="96x96" href="/icons/favicon-96x96.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+      <link rel="manifest" href="/icons/manifest.json" />
+      <meta name="msapplication-TileColor" content="#ffffff" />
+      <meta name="msapplication-TileImage" content="/icons/ms-icon-144x144.png" />
+      <meta name="theme-color" content="#ffffff" />
       <link rel="canonical" href={useSlash(siteConfig.siteUrl)} />
     </Helmet>
     <Navigation
